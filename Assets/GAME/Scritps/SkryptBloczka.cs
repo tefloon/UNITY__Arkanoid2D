e03419239553a4200d9ffacfa0a1e455e3644b15
Antoni Gawlikowski <tefloon@gmail.com>
2019-07-15 00:12:39 +0200

    Dodano: # Prefabrykat bloczka # Skrypt układający bloczki na mapie # Skrypt dla bloczka umożliwiający jego zbicie # Dodatkowe opcje w skrypcie bloczka: # # Bloczki mogą mieć "życie" - aby je zbić trzeba je trafić więcej niż raz # # W zależności od ilości "żyć", które pozostały, zmienia się przeźroczystość bloczka
    Dodatkowo uporządkowano hierarchię.

---
+++ Assets/GAME/Scritps/SkryptBloczka.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkryptBloczka : MonoBehaviour
+{
+	[SerializeField] int liczbaZyc = 3;
+
+	int obecneZycia;
+	SpriteRenderer sr;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Ball"))
+		{
+			obecneZycia--;
+			UaktualnijKolor();
+
+			if (obecneZycia <= 0)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	private void UaktualnijKolor()
+	{
+		float a = ((float)obecneZycia / (float)liczbaZyc);
+		sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, a);
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		obecneZycia = liczbaZyc;
+	}
+
+
+}
---
+++ Assets/GAME/Scritps/TworzenieMapy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TworzenieMapy : MonoBehaviour
+{
+	[Header("Obiekty")]
+	[SerializeField] GameObject prefabBloczka;
+	[SerializeField] Transform start;
+	[SerializeField] Transform rodzicBloczkow;
+
+	[Header("Ustawienia")]
+	[SerializeField] int liczbaKolumn = 20;
+	[SerializeField] int liczbaRzedow = 5;
+
+	[SerializeField] float odstepX;
+	[SerializeField] float odstepY;
+
+	void Start()
+	{
+		float szerBloczka = prefabBloczka.transform.localScale.x;
+		float wysBloczka = prefabBloczka.transform.localScale.y;
+
+		for (int y = 0; y < liczbaRzedow; y++)
+		{
+			for (int x = 0; x < liczbaKolumn; x++)
+			{
+				Vector2 tilePlace = new Vector2(x * (szerBloczka + odstepX), -y * (wysBloczka + odstepY)) 
+									+ (Vector2)start.position;
+
+				Instantiate(prefabBloczka, tilePlace, Quaternion.identity, rodzicBloczkow);
+			}
+		}
+	}
+}
